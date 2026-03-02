2ed82cb0ce40bb9dc24338c0bcd24b01a2525909
haku <rootlogic7@proton.me>
2026-03-03 00:11:58 +0100

    adding hyprland config with home manager in desktop/default.nix

---
+++ home/desktop/default.nix
@@ -0,0 +1,57 @@
+{ config, pkgs, ... }:
+
+{
+  wayland.windowManager.hyprland = {
+    enable = true;
+    
+    # hyprland.conf
+    settings = {
+      
+      # 1. Monitore
+      monitor = ",preferred,auto,1";
+
+      # 2. Tastatur
+      input = {
+        kb_layout = "de";
+        kb_variant = "nodeadkeys";
+        follow_mouse = 1;
+      };
+
+      # 3. Variablen
+      "$mod" = "SUPER";
+      "$terminal" = "foot";
+      "$menu" = "fuzzel";
+
+      # 4. Tastenkürzel (Binds)
+      bind = [
+        # Programme starten
+        "$mod, Return, exec, $terminal"
+        "$mod, Space, exec, $menu"
+        
+        # Fenster-Management
+        "$mod, Q, killactive,"
+        "$mod, F, fullscreen,"
+        "$mod SHIFT, Space, togglefloating,"
+        
+        # Hyprland beenden (Ausloggen)
+        "$mod SHIFT, E, exit,"
+
+        # Fokus wechseln (mit den Pfeiltasten)
+        "$mod, h, movefocus, l"
+        "$mod, j, movefocus, d"
+        "$mod, k, movefocus, u"
+        "$mod, l, movefocus, r"
+      ];
+
+      # Workspaces (Arbeitsflächen) wechseln
+      bind = [
+        "$mod, 1, workspace, 1"
+        "$mod, 2, workspace, 2"
+        "$mod, 3, workspace, 3"
+        "$mod, 4, workspace, 4"
+        "$mod SHIFT, 1, movetoworkspace, 1"
+        "$mod SHIFT, 2, movetoworkspace, 2"
+      ];
+    };
+  };
+}
