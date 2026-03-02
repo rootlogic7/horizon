b89305124a765b2bff2699bcd11e0eabc2902509
haku <rootlogic7@proton.me>
2026-03-02 14:39:04 +0100

    Refactor home.nix structure and group settings

--- home/haku.nix
+++ home/haku.nix
@@ -2,45 +2,50 @@
 
 {
   # Grundlegende Home-Manager-Einstellungen
-  home.username = "haku";
-  home.homeDirectory = "/home/haku";
+  home = {
+
+    # home.stateVersion
+    stateVersion = "25.11";
+
+    username = "haku";
+    homeDirectory = "/home/haku";
   
-  # Diese Version muss (genau wie in der systemweiten config) auf 25.11 bleiben
-  home.stateVersion = "25.11";
 
-  # 📦 Das Basis-Arsenal für unseren "Min-Maxing" Ansatz
-  home.packages = with pkgs; [
-    # Wayland & Desktop
-    hyprpaper
-    hypridle
-    waybar
-    mako
-    fuzzel
+    # home.packages
+    packages = with pkgs; [
+      # Wayland & Desktop
+      hyprpaper
+      hypridle
+      waybar
+      mako
+      fuzzel
     
-    # Terminal & Workflow
-    foot
-    zellij
-    yazi
+      # Terminal & Workflow
+      foot
+      zellij
+      yazi
 
-    # Rust-basierte CLI-Tools (Die modernen Alternativen)
-    eza      # Moderner ls-Ersatz
-    bat      # Moderner cat-Ersatz
-    zoxide   # Smarter cd-Ersatz
-    ripgrep  # Extrem schnelles grep
-    fd       # Schnelleres find
-    bottom   # Moderner Systemmonitor (htop-Ersatz)
+      # Rust-basierte CLI-Tools (Die modernen Alternativen)
+      eza      # Moderner ls-Ersatz
+      bat      # Moderner cat-Ersatz
+      zoxide   # Smarter cd-Ersatz
+      ripgrep  # Extrem schnelles grep
+      fd       # Schnelleres find
+      bottom   # Moderner Systemmonitor (htop-Ersatz)
 
-    # Web & Development
-    firefox
-    git
-    wget
-    curl
-  ];
+      # Web & Development
+      firefox
+      git
+      wget
+      curl
+    ];
+  };
 
-  # Home-Manager darf sich selbst verwalten
-  programs.home-manager.enable = true;
+  programs = {
+    home-manager.enable = true;
+  };
 
-  # Später, wenn das System läuft, werden wir hier unsere Module importieren:
+  # Später:
   # imports = [
   #   ./shell/default.nix
   #   ./desktop/default.nix
